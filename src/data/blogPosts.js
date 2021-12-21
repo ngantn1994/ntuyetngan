e3163a5bf5c19eb04b3387ff9accf49d8dddb06b
ngantn1994 <ngantn1994@gmail.com>
2021-12-22 01:47:02 +0900

    Add new blog post

--- src/data/blogPosts.js
+++ src/data/blogPosts.js
@@ -2,8 +2,13 @@ import blog1 from 'raw-loader!./posts/blog1.txt';
 import blog2 from 'raw-loader!./posts/blog2.txt';
 import blog3 from 'raw-loader!./posts/blog3.txt';
 import blog4 from 'raw-loader!./posts/blog4.txt';
+import blog4 from 'raw-loader!./posts/blog5.txt';
 
 const blogPosts = [
+  {
+    id: 5,
+    html: blog5,
+  },
   {
     id: 4,
     html: blog4,
